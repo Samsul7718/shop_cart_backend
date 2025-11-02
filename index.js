c191b43cfbee0ce4392f56e85b4d7ed0b1a0d7c8
samsul alam <samsul7718@gmail.com>
2025-11-02 19:50:51 +0530

    added express urlencoded

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(cors(
 }
 ));
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 const port =process.env.PORT || 3000
 
@@ -32,17 +33,32 @@ app.get("/api/product",(req,res)=>{
     res.send(products)
 })
 
-app.post("/api/order", (req, res) => {
-  const rozorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-  });
-  console.log("Order endpoint hit");
-  const { products,total } = req.body;
-  console.log("Received Order:", products);
-  console.log("Total Payble Amount:", total);
-  res.json({ message: "Order received successfully!",products,total});
+app.post("/api/order", async(req, res) => {
+  try {
+    const rozorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+    const options =  req.body;
+    const order =await rozorpay.orders.create(options);
+
+    if(!order){
+      return res.status(500).send("Some error occured");
+    }
+    res.json(order);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+    
+  }
 });
+  // console.log("Order endpoint hit");
+  // const { products,total } = req.body;
+  // console.log("Received Order:", products);
+  // console.log("Total Payble Amount:", total);
+  // res.json({ message: "Order received successfully!",products,total});
+// });
 
 app.listen(port,()=>{
     console.log(`Server run at http://localhost:${port}`)
