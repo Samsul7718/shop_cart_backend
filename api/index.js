7c6b4fa53803c7a94cbe4f3b03dca67cbaca65f7
samsul alam <samsul7718@gmail.com>
2025-11-04 14:47:26 +0530

    added data url

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app=express()
 app.use(cors(
     {
   origin: [
-    "https://shop-cart-frontend.vercel.app/",
+    "https://shop-cart-frontend.vercel.app",
       "http://localhost:5173",
        "http://localhost:5174"  
 
