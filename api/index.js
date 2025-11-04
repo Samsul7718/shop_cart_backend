86362566f34a01a2131bc5a015b6106c4cb55153
samsul alam <samsul7718@gmail.com>
2025-11-04 11:32:59 +0530

    all file modified

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app=express()
 app.use(cors(
     {
   origin: [
-    "https://shop-cart-frontend.vercel.app",
+    "http://localhost:3000",
       "http://localhost:5173",
        "http://localhost:5174"  
   ], 
